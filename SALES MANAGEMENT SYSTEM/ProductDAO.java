539064963bd01cfc287c955c1745028d633ee5e3
PIYUSH LENDE <piyushlende93@gmail.com>
2026-04-06 16:35:34 +0530

    ..

--- SALES MANAGEMENT SYSTEM/ProductDAO.java
+++ SALES MANAGEMENT SYSTEM/ProductDAO.java
@@ -11,7 +11,7 @@ public class ProductDAO {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             con = DriverManager.getConnection(
-                "jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
+                "jdbc:oracle:thin:@localhost:1521:XE", "", "");
         } catch (Exception e) {
             e.printStackTrace();
         }
